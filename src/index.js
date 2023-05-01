df859557c676c6cd8b0cb3b0195b335cd1a78d48
Nandani Gajjar <86545697+nandani6501@users.noreply.github.com>
2023-05-01 23:27:48 +0530

    added show table

--- src/index.js
+++ src/index.js
@@ -7,13 +7,24 @@ import {
 } from "react-router-dom";
 import './index.css';
 import App from './App';
+import ShowTable from './pages/user/Component/showTable.js'
 import reportWebVitals from './reportWebVitals';
+import ViewTransaction from './pages/user/Component/transaction';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/showTable",
+    element:<ShowTable/>
+  },
+  {
+    path: `/transaction/:id`,
+    element:<ViewTransaction/>
+  },
+  
  
   
 ]);
